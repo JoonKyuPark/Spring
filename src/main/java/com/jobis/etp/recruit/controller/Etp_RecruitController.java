e2e3ab42917a18cbb05ad02a5ce2b553e391b8f8
JeoungHyunWoo <god31@DESKTOP-AO53SU1>
2016-11-18 02:33:06 +0900

    commit

--- src/main/java/com/jobis/etp/recruit/controller/Etp_RecruitController.java
+++ src/main/java/com/jobis/etp/recruit/controller/Etp_RecruitController.java
@@ -1,8 +1,45 @@
 package com.jobis.etp.recruit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/etp/etp_log/*")
 public class Etp_RecruitController {
+	
+	
+	
+	@RequestMapping("test")
+	public void insert_form(){
+		
+	}
+	
+	
+
+	
+	
+
+	
+	@RequestMapping(value ="/member_detail" ,method=RequestMethod.GET)
+	public String detail(Model model , @RequestParam int bno)throws Exception{
+
+		
+
+		
+		System.out.println("bno น๘ศฃดย "+bno);
+		
+		model.addAttribute("bno", bno);
+		
+		return "/etp/etp_log/member_detail";
+		
+	}
+	
+
+	
+	
+	
 
 }
--- src/main/java/com/jobis/etp/resume/controller/Etp_ResumeController.java
+++ src/main/java/com/jobis/etp/resume/controller/Etp_ResumeController.java
@@ -1,8 +1,35 @@
 package com.jobis.etp.resume.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jobis.etp.recruit.domain.MemberVO;
+import com.jobis.etp.recruit.persistance.MemberDAO;
 
 @Controller
+@RequestMapping("/sboard/*")
 public class Etp_ResumeController {
 
+	@Inject
+	private MemberDAO dao;
+	
+		@RequestMapping(value="/home", method=RequestMethod.GET)
+		public void testGetTime() {
+			System.out.println(dao.getTime());
+		}
+
+		/*@RequestMapping(value="/register", method=RequestMethod.GET)
+		public void testInsertMember() {
+			MemberVO vo = new MemberVO();
+			vo.setUserid("user10");
+			vo.setUserpw("1234");
+			vo.setUsername("ȫ�浿");
+			vo.setEmail("aa@aa.com");
+			
+			dao.insertMember(vo);
+		
+		}*/
 }
